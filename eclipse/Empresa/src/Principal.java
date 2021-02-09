8684c2808dfc5d04860435992b2e09a1cf2b821b
José MIguel <josemical43@gmail.com>
2021-02-09 01:41:27 +0100

    a

--- eclipse/Empresa/src/Principal.java
+++ eclipse/Empresa/src/Principal.java
@@ -8,12 +8,17 @@ public class Principal {
 			empleado1.emp_no = 1;
 			empleado1.oficio = "contable";
 			empleado1.fecha_alt = "11-01-2009";
+			empleado1.salario = 1123.80;
 			
 		empleado empleado2 = new empleado();
 			empleado2.apellido = "tornes";
 			empleado2.emp_no = 2;
 			empleado2.oficio ="programador";
 			empleado2.fecha_alt = "15-11-2008";
+			empleado2.salario = 3.01;
+			
+		empleado1.datoEmpleado();
+		empleado2.datoEmpleado();
 
 	}
 
--- eclipse/Empresa/src/empleado.java
+++ eclipse/Empresa/src/empleado.java
@@ -1,8 +1,18 @@
 
 public class empleado {
-	public double emp_no;
+	public int emp_no;
 	public String apellido;
 	public String oficio;
 	public String fecha_alt;
 	public double salario;
+	
+	public void datoEmpleado() {
+		System.out.println("-El número de empleado es      : " + this.emp_no);
+		System.out.println("-El Apellido de empleado es    : " + this.apellido);
+		System.out.println("-El oficio de empleado es      : " + this.oficio);
+		System.out.println("-Su fecha de alta tuvo lugar el: " + this.fecha_alt);
+		System.out.println("-Le corresponde un salario de  : " + this.salario);
+		System.out.println();
+		System.out.println();
+	}
 }
