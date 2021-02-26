ea6aba69f4e37882877db494e15d7efe9f210447
José MIguel <josemical43@gmail.com>
2021-02-26 11:29:57 +0100

    a

---
+++ eclipse/Composicion2/src/departamento.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class departamento {
+	
+	public int getDept_no() {
+		return dept_no;
+	}
+
+	public void setDept_no(int dept_no) {
+		this.dept_no = dept_no;
+	}
+
+	public String getdNombre() {
+		return dNombre;
+	}
+
+	public void setdNombre(String dNombre) {
+		this.dNombre = dNombre;
+	}
+
+	public String getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+
+	
+
+	int dept_no;
+	String dNombre;
+	String localizacion;
+	
+	/**private empleado personal[];
+	
+	public departamento(int dept_no, String dNombre, String localizacion, empleado m[]) {
+		personal = m;
+		this.dept_no = dept_no;
+		this.dNombre = dNombre;
+		this.localizacion = localizacion;
+		
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "departamento [dept_no=" + dept_no + ", dNombre=" + dNombre + ", localizacion=" + localizacion
+				+ ", personal=" + Arrays.toString(personal) + "]";
+	}
+
+	
+	**/
+
+	
+	public departamento(int dept_no, String dNombre, String localizacion) {
+		this.dept_no = dept_no;
+		this.dNombre = dNombre;
+		this.localizacion = localizacion;
+		
+		empleado[] empleados = new empleado[5];
+		empleados[1] = new empleado(0, "Paco", null, null, 0);
+		empleados[2] = new empleado(2, "Manolo", null, null, 0);
+		
+		
+	}
+	
+
+	
+	
+
+}
---
+++ eclipse/Composicion2/src/principal.java
@@ -0,0 +1,35 @@
+
+public class principal {
+
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public static void metodoAgregacion() {
+		/**
+		departamento[] departamentos = new departamento[5];
+		
+		empleado[] empleados = new empleado[5];
+		
+		empleados[1] = new empleado(0, "Paco", null, null, 0);
+		empleados[2] = new empleado(2, "Manolo", null, null, 0);
+		
+		departamentos[1] = new departamento(1,"Matemáticas" , "Piso Bajo", empleados);
+		
+		
+		System.out.println(departamentos[1]);
+		**/
+	}
+	
+	public static void metodoComposicion() {
+		
+		departamento[] departamentos = new departamento[5];
+
+		
+		departamentos[1] = new departamento(1,"Matemáticas" , "Piso Bajo");
+
+		
+	}
+}
